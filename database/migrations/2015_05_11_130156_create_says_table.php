3b9f33befb16eddc99ad866774112c3b5f0bd7ab
JasonPan <jason8399@gmail.com>
2015-05-11 21:18:30 +0800

    add Says model, migration

---
+++ database/migrations/2015_05_11_130156_create_says_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSaysTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('says', function(Blueprint $table)
+		{
+			$table->string('Comment_UUID');
+			$table->foreign('Comment_UUID')
+			->references('Comment_UUID')->on('comments')
+			->onDelete('cascade');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')
+			->references('id')->on('users')
+			->onDelete('cascade');
+			$table->primary(['Comment_UUID', 'user_id']);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('says');
+	}
+
+}
