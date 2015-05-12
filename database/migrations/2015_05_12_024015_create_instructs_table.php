fa8d298406e0a9d1254a5283745b0aa43c3b9df0
JasonPan <jason8399@gmail.com>
2015-05-12 10:52:32 +0800

    add Instructs model, migration

---
+++ database/migrations/2015_05_12_024015_create_instructs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInstructsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('instructs', function(Blueprint $table)
+		{
+			$table->integer('Teacher_ID')->unsigned();
+			$table->foreign('Teacher_ID')
+			->references('id')->on('teachers')
+			->onDelete('cascade');
+			$table->string('Course_No');
+			$table->foreign('Course_No')
+			->references('Course_No')->on('courses')
+			->onDelete('cascade');
+			$table->primary(['Teacher_ID', 'Course_No']);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('instructs');
+	}
+
+}
