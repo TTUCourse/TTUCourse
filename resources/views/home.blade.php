e5abc183c0b26d8fb590c95ee18b5f005dfe8aba
JasonPan <jason8399@gmail.com>
2015-05-26 21:39:29 +0800

    add script in home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,26 @@
 @extends('app')
 
+@section('title', 'Home')
+
 @section('content')
 <div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">Home</div>
-
 				<div class="panel-body">
-					You are logged in!
+					  You are logged in!
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
+@stop
+@section('script')
+<script>
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+  $('.parallax').parallax();
+});
+</script>
 @endsection
