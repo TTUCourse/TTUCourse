7663a09d22def62a5224d50f8bc0edc09a2ebc2e
JasonPan <jason8399@gmail.com>
2015-07-31 06:47:53 +0800

    Fix after change password can't login bug, fix regex error.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,7 +42,7 @@ class UserController extends Controller {
 
 		if($request->has('password'))
 		{
-			$user->password = $request->input('password');
+			$user->password = bcrypt($request->input('password'));
 		}
 
 		if($user->nickname != $request->input('nickname'))
